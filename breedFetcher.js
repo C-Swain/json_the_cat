e744a683d028acceb8c1cee71f3b48eefc8027f4
C-Swain <catherinemexicoswain@gmail.com>
2021-09-23 22:43:30 +0000

    kitty Test

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,15 +2,20 @@ const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => {
 
-const link =	`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-request(link, (error, response, body) => {
+  const link =	`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(link, (error, response, body) => {
   
-  if (error) 
-	{return callback(error);}
-
-  const data = JSON.parse(body);
-  return callback(null, data[0].description);
-});
+    if (error) {
+      return callback(error);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback(' Breed Not valid');
+      } else {
+        return callback(null, data[0].description);
+      }
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
